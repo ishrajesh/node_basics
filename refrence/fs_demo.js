c9715daf7409ffb459cc1c376837d7976b34b147
ishrajesh <ishrajeshshelley@gmail.com>
2023-01-14 22:12:23 +0530

    append a file

--- refrence/fs_demo.js
+++ refrence/fs_demo.js
@@ -1,14 +1,20 @@
  const fs = require('fs');
  const path = require('path');
 
- //Create folder
- fs.mkdir(path.join(__dirname, '/test'),{},err=> {
-    if(err) throw err;
-    console.log('Folder created');
-  });
+//  //Create folder
+//  fs.mkdir(path.join(__dirname, '/test'),{},err=> {
+//     if(err) throw err;
+//     console.log('Folder created');
+//   });
 
  //Open and write to file
  fs.writeFile(path.join(__dirname, '/test', 'hello.txt') , 'Hello World!!', err=> {
     if(err) throw err;
     console.log('File has been created');
+  });
+
+ //Append to file
+ fs.appendFile(path.join(__dirname, '/test', 'hello.txt') , '\nHa Ha Ha ', err=> {
+    if(err) throw err;
+    console.log('File has been appended');
   });
