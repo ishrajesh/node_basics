1a76fd28d628087195e23a505e261338772e1d91
ishrajesh <ishrajeshshelley@gmail.com>
2023-01-14 22:20:51 +0530

    fs module: readFile, rename

--- refrence/fs_demo.js
+++ refrence/fs_demo.js
@@ -1,5 +1,5 @@
- const fs = require('fs');
- const path = require('path');
+const fs = require('fs');
+const path = require('path');
 
 //  //Create folder
 //  fs.mkdir(path.join(__dirname, '/test'),{},err=> {
@@ -7,14 +7,30 @@
 //     console.log('Folder created');
 //   });
 
- //Open and write to file
- fs.writeFile(path.join(__dirname, '/test', 'hello.txt') , 'Hello World!!', err=> {
-    if(err) throw err;
-    console.log('File has been created');
-  });
+//  //Open and write to file
+//  fs.writeFile(path.join(__dirname, '/test', 'hello.txt') , 'Hello World!!', err=> {
+//     if(err) throw err;
+//     console.log('File has been created');
+//   });
 
- //Append to file
- fs.appendFile(path.join(__dirname, '/test', 'hello.txt') , '\nHa Ha Ha ', err=> {
+//  //Append to file
+//  fs.appendFile(path.join(__dirname, '/test', 'hello.txt') , '\nHa Ha Ha ', err=> {
+//     if(err) throw err;
+//     console.log('File has been appended');
+//   });
+
+
+//  //Read file
+// fs.readFile(path.join(__dirname, '/test','hello.txt'),'utf8',(err,data)=> {
+//   if(err) throw err;
+//   console.log(data);
+// });
+
+//Rename a file 
+fs.rename(
+  path.join(__dirname, '/test','hello.txt'),
+  path.join(__dirname,'/test','helloworld.txt'),
+  err=> {
     if(err) throw err;
-    console.log('File has been appended');
-  });
+    console.log('renaming the file');
+});
