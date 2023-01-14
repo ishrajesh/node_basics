dc1887232380d9246136e07436164a8cb33500c7
ishrajesh <ishrajeshshelley@gmail.com>
2023-01-14 22:41:10 +0530

    os related methods

---
+++ refrence/os_demo.js
@@ -0,0 +1,25 @@
+const os = require('os');
+
+//Platform
+console.log(os.platform());
+
+//CPU Arch
+console.log(os.arch());
+
+//CPU Arch
+console.log(os.cpus());
+
+//Free Memory
+console.log(os.freemem());
+
+//Total Memory
+console.log(os.totalmem());
+
+//Home Directory
+console.log(os.homedir());
+
+//Uptime
+console.log(os.uptime());
+
+
+
